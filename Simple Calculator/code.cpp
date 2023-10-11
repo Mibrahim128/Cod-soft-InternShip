4892049035d2dfa7c6388236d034b7e1eb3c5e39
Mibrahim128 <143089792+Mibrahim128@users.noreply.github.com>
2023-10-11 12:33:56 +0500

    Add files via upload

---
+++ Simple Calculator/code.cpp
@@ -0,0 +1,43 @@
+#include<iostream>
+#include<conio.h>
+using namespace std;
+int main()
+{
+	char oper;
+	int a, b;
+	cout << " Enter the Operator + - * / ";
+	cin >> oper;
+	cout << " Enter the 1st Number ";
+	cin >> a;
+	cout << " Enter the 2nd Number ";
+	cin >> b;
+
+	switch (oper)
+	{ 
+	   case '+':
+		   cout << a << " + " << b <<" = " << a + b;
+		   break;
+	   case '-':
+		   cout << a << " - " << b << " = " << a - b;
+		   break;
+	   case '*':
+		   cout << a << " * " << b << " = " << a * b;
+		   break;
+	   case '/':
+		   if (b != 0)
+		   {
+			   cout << a << " / " << b << " = " << a / b;
+		   }
+		   else
+		   {
+			   cout << " Division is not possible ";
+		   }
+		   break;
+	  
+	   default:
+	
+		   cout << "Error! operator is not correct";
+		   break;
+	}
+	return 0;
+}
